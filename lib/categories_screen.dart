33a3c28b3e91e79f71046b8e9f9a6a29e2ab2038
SoulAndBone <soulandbone@gmail.com>
2022-09-26 16:48:01 +0300

    GridView, HomeScreen changed for Categories Screen, and data displayed on the screen, Category model created

---
+++ lib/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'category_item.dart';
+import './dummy_data.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DeliMeals'),
+        ),
+        body: GridView(
+            children: DUMMY_CATEGORIES
+                .map((e) => CategoryItem(e.title, e.color))
+                .toList(),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20)));
+  }
+}
