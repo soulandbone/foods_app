6f5543b6f55ba6545222924a47953f51cbf5870d
SoulAndBone <soulandbone@gmail.com>
2022-10-04 11:38:47 +0300

    Meals are displayed according to the category that was selected. First Rough approach to just show a list of titles of the meals, nothing else

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   //this is the screen that opens after the user has clicked on one of the categories in the GridView
@@ -10,14 +11,21 @@ class CategoryMealsScreen extends StatelessWidget {
     final arguments =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
 
+    final mealCategoryFiltered = DUMMY_MEALS.where((element) {
+      return element.categories.contains(arguments['id']);
+    }).toList(); //generates a new list from DUMMY_MEALS where the element has the selected category as one of the elements in the categories the food belongs to.  This list then only has the meals that belong to a particular category, so its just a matter of rendering those elements to the screen
+
+    print(mealCategoryFiltered.length);
     final title = arguments['title'];
 
     return Scaffold(
       appBar: AppBar(title: Text(title!)),
-      body: Center(
-          child: Container(
-        child: Text('This is the meal'),
-      )),
+      body: ListView.builder(
+        itemBuilder: ((context, index) {
+          return Container(child: Text(mealCategoryFiltered[index].title));
+        }),
+        itemCount: mealCategoryFiltered.length,
+      ),
     );
   }
 }
