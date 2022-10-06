4d15160a49062be0329d68fb53cc22ce3755dbfe
SoulAndBone <soulandbone@gmail.com>
2022-10-06 11:05:25 +0300

    Now the meal Item displays image, title , and also the duration, affordability and complexity

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -18,6 +18,38 @@ class MealItem extends StatelessWidget {
 
   void selectMeal() {}
 
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+
+      case Affordability.Pricey:
+        return 'Pricey';
+
+      case Affordability.Luxurious:
+        return 'Luxurious';
+
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+
+      case Complexity.Hard:
+        return 'Hard';
+
+      case Complexity.Challenging:
+        return 'Challenging';
+
+      default:
+        return 'Unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -32,7 +64,7 @@ class MealItem extends StatelessWidget {
                 Stack(
                   children: [
                     ClipRRect(
-                      borderRadius: BorderRadius.only(
+                      borderRadius: const BorderRadius.only(
                           topLeft: Radius.circular(15),
                           topRight: Radius.circular(15)),
                       child: Image.network(
@@ -43,18 +75,61 @@ class MealItem extends StatelessWidget {
                       ),
                     ),
                     Positioned(
-                      bottom: 10,
+                      bottom: 20,
                       right: 10,
-                      child: Text(
-                        title,
-                        style: const TextStyle(
-                            fontSize: 20,
+                      child: Container(
+                        width: 300,
+                        color: Colors.black54,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        child: Text(
+                          textAlign: TextAlign.right,
+                          title,
+                          overflow: TextOverflow.fade,
+                          softWrap: true,
+                          style: const TextStyle(
+                            fontFamily: 'Raleway',
+                            fontSize: 24,
                             color: Colors.white,
-                            backgroundColor: Colors.black54),
+                          ),
+                        ),
                       ),
                     )
                   ],
                 ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.schedule),
+                          const SizedBox(
+                            width: 6,
+                          ),
+                          Text('$duration min')
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.work),
+                          const SizedBox(
+                            width: 6,
+                          ),
+                          Text(complexityText)
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.attach_money),
+                          const SizedBox(width: 6),
+                          Text(affordabilityText)
+                        ],
+                      )
+                    ],
+                  ),
+                )
               ],
             )));
   }
